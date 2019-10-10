2a8731306040e93db68e9771eb6c2be11702e36e
Dreiko <dreiko@inbox.ru>
2019-10-10 14:28:18 +0200

    angular lesson 15

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-	toggle: any = false;
+	arr = [1, 1, 2, 3, 5, 8]
 
+	objs = [
+		{title: 'Post1', author: 'Alex', comments: [
+			{name: 'Arnold', text: 'You are the best!'},
+			{name: 'Denis', text: 'You can do everything you want with your life!'},
+		]},
+		{title: 'Post2', author: 'Alex', comments: [
+			{name: 'Frank', text: 'You are winner!'},
+		]},
+		
+	]
 
 }
