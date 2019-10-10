ea3fb004365a168b4832571295efc33049be7393
Dreiko <dreiko@inbox.ru>
2019-10-10 14:49:10 +0200

    angular lesson 16

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,17 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-	arr = [1, 1, 2, 3, 5, 8]
-
-	objs = [
-		{title: 'Post1', author: 'Alex', comments: [
-			{name: 'Arnold', text: 'You are the best!'},
-			{name: 'Denis', text: 'You can do everything you want with your life!'},
-		]},
-		{title: 'Post2', author: 'Alex', comments: [
-			{name: 'Frank', text: 'You are winner!'},
-		]},
-		
-	]
+	now: Date = new Date()
 
 }
