5e035096fbd8ce4faf5c9b677ecb57d3bde28d05
Dreiko <dreiko@inbox.ru>
2019-10-11 17:39:01 +0200

    angular components lesson 8

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,10 +1,11 @@
-import { Component, Input, Output, EventEmitter, OnInit, ContentChild, ElementRef, OnChanges, SimpleChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy } from '@angular/core';
+import { Component, ChangeDetectionStrategy, Input, Output, EventEmitter, OnInit, ContentChild, ElementRef, OnChanges, SimpleChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy } from '@angular/core';
 import { Post } from '../app.component'
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
-  styleUrls: ['./post.component.sass']
+  styleUrls: ['./post.component.sass'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PostComponent implements 
 	OnInit, 
