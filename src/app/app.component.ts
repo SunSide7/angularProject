28e35ba3f28097c222ca3cbc4a4698cbedc67c58
Dreiko <dreiko@inbox.ru>
2019-10-11 19:14:28 +0200

    angular services lesson 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AppCounterService } from './services/app-counter.service'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
+
+	constructor(private appCounterService: AppCounterService) {
+
+	}
 
 }
