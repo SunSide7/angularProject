d0f811a9ae659e8bbbf7f42c5133dc9e47d79255
Dreiko <dreiko@inbox.ru>
2019-10-11 12:26:38 +0200

    angular components lesson 3

--- src/app/post-form/post-form.component.ts
+++ src/app/post-form/post-form.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+
+import { Post } from '../app.component';
 
 @Component({
   selector: 'app-post-form',
@@ -7,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostFormComponent implements OnInit {
 
-  constructor() { }
+	@Output() onAdd: EventEmitter<Post> = new EventEmitter()
 
-  ngOnInit() {
-  }
+	title = ''
+	text = ''
+
+    constructor() { }
+
+    ngOnInit() {
+    }
+
+    addPost() {
+    	if (this.text.trim() && this.title.trim()) {
+    		const post: Post = {
+    			title: this.title,
+    			text: this.text
+    		}
+
+	    	this.onAdd.emit(post)
+
+	    	this.title = this.text = ''
+    	}
+
+    }
 
 }
