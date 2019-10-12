3d90d82f841200e5df974a7b5cd0acf0d5692ee3
Dmitry Ascended <DieMon91@list.ru>
2019-10-12 20:41:58 +0200

    angular routing start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,11 @@
-import { Component, OnInit } from '@angular/core'
-import { FormGroup, FormControl, Validators } from '@angular/forms'
+import {Component} from '@angular/core'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-	form: FormGroup
-
-	ngOnInit() {
-		this.form = new FormGroup({
-			email: new FormControl('', [
-				Validators.email, 
-				Validators.required
-			]),
-			password: new FormControl(null, [
-				Validators.required,
-				Validators.minLength(7)
-			])
-		})
-	}
-
-	submit() {
-		if (this.form.valid) {
-			console.log('Form:', this.form)
-			const formData = {...this.form.value}
-
-			console.log('Form Data:', formData)
-		}
-	}
+export class AppComponent {
+  constructor() {}
 }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,27 @@ import {BrowserModule} from '@angular/platform-browser'
 import {NgModule} from '@angular/core'
 
 import {AppComponent} from './app.component'
-import {FormsModule, ReactiveFormsModule} from '@angular/forms'
+import {FormsModule} from '@angular/forms'
+import {AboutComponent} from './about/about.component'
+import {HomeComponent} from './home/home.component'
+import {PostsComponent} from './posts/posts.component'
+import {PostComponent} from './post/post.component'
+import {AboutExtraComponent} from './about-extra/about-extra.component'
 
 @NgModule({
   declarations: [
     AppComponent,
+    AboutComponent,
+    HomeComponent,
+    PostsComponent,
+    PostComponent,
+    AboutExtraComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    ReactiveFormsModule
   ],
-  providers: [
-
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {
